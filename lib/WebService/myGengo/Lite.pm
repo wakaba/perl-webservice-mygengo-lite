010550250cf60afd880d7977f3adfb2f62e7cf62
wakabatan <wakabatan@localhost.localdomain>
2012-03-22 13:45:54 +0900

    job apis

--- lib/WebService/myGengo/Lite.pm
+++ lib/WebService/myGengo/Lite.pm
@@ -97,7 +97,7 @@ sub request {
 #
 # $res->data = [{lc => langtag, language => ..., localized_name => ...,
 #                unit_type => ...}]
-sub service_languages {
+sub service_langs {
     my ($self, %args) = @_;
     return $self->request(
         path => q<translate/service/languages>,
@@ -108,7 +108,7 @@ sub service_languages {
 #
 # $res->data = [{lc_src => langtag, lc_tgt => langtag,
 #                unit_price => ..., tier => ...}]
-sub service_language_pairs {
+sub service_lang_pairs {
     my ($self, %args) = @_;
     return $self->request(
         path => q<translate/service/language_pairs>,
@@ -149,8 +149,20 @@ sub create_job_request ($%) {
   return bless {@_}, 'WebService::myGengo::Lite::Job';
 } # create_job_request
 
+## <http://mygengo.com/api/developer-docs/methods/translate-jobs-post/>.
+sub job_post {
+  my ($self, $jobs, %args) = @_;
+  return $self->request
+      (method => 'POST',
+       path => q<translate/jobs>,
+       data => {
+         jobs => [map { $_->as_jsonable } @$jobs],
+         as_group => $args{as_group} || 0,
+       });
+} # job_post
+
 ## <http://mygengo.com/api/developer-docs/methods/translate-jobs-get/>.
-sub jobs_get {
+sub job_list {
   my ($self, %args) = @_;
   return $self->request
       (method => 'GET',
@@ -160,19 +172,26 @@ sub jobs_get {
          timestamp_after => $args{timestamp_after},
          count => $args{count},
        });
-} # jobs_get
+} # job_list
 
-## <http://mygengo.com/api/developer-docs/methods/translate-jobs-post/>.
-sub jobs_post {
-  my ($self, $jobs, %args) = @_;
+## <http://mygengo.com/api/developer-docs/methods/translate-job-id-get/>.
+sub job_get ($$%) {
+  my ($self, $job_id, %args) = @_;
   return $self->request
-      (method => 'POST',
-       path => q<translate/jobs>,
+      (method => 'GET',
+       path => q<translate/job/> . $job_id,
        data => {
-         jobs => [map { $_->as_jsonable } @$jobs],
-         as_group => $args{as_group} || 0,
+         pre_mt => $args{pre_mt} || 0,
        });
-} # jobs_post
+}
+
+## <http://mygengo.com/api/developer-docs/methods/translate-jobs-ids-get/>.
+sub jobs_get ($$%) {
+  my ($self, $job_ids, %args) = @_;
+  return $self->request
+      (method => 'GET',
+       path => q<translate/jobs/> . join ',', @$job_ids);
+}
 
 ## <http://mygengo.com/api/developer-docs/methods/translate-service-quote-post/>.
 sub quote {
@@ -188,17 +207,6 @@ sub quote {
     # {jobs => [{eta => ..., credits => ..., unit_count => ...}]}
 } # quote
 
-## <http://mygengo.com/api/developer-docs/methods/translate-job-id-get/>.
-sub job_get ($$%) {
-  my ($self, $job_id, %args) = @_;
-  return $self->request
-      (method => 'GET',
-       path => q<translate/job/> . $job_id,
-       data => {
-         pre_mt => $args{pre_mt},
-       });
-}
-
 ## <http://mygengo.com/api/developer-docs/methods/translate-job-id-preview-get/>.
 sub job_preview ($$%) {
     my ($self, $job_id, %args) = @_;
@@ -210,6 +218,22 @@ sub job_preview ($$%) {
 package WebService::myGengo::Lite::Job;
 
 ## <http://mygengo.com/api/developer-docs/payloads/>.
+
+sub new_from_json_job {
+    my ($class, $job) = @_;
+    
+    $job->{source}->{body} = delete $job->{body_src};
+    $job->{source}->{lang} = delete $job->{lc_src};
+    
+    $job->{target}->{lang} = delete $job->{lc_tgt};
+    $job->{target}->{preview_image_url} = delete $job->{preview_url};
+    
+    $job->{quote}->{unit_count} = delete $job->{unit_count};
+    $job->{quote}->{eta} = delete $job->{eta};
+    $job->{quote}->{credits} = delete $job->{credits};
+    
+    return bless $job, $class;
+}
 
 sub as_jsonable ($) {
   my $self = shift;
@@ -279,6 +303,13 @@ sub new_from_lwp_res ($$;%) {
             $args{input_jobs}->[$_]->{$args{job_data_key}} = $result->{data}->{jobs}->[$_];
         }
         $result->{jobs} = $args{input_jobs};
+    } elsif ($result->{data} and ref $result->{data} eq 'HASH' and
+             $result->{data}->{jobs} and
+             ref $result->{data}->{jobs} eq 'ARRAY') {
+        $result->{jobs} = [map { WebService::myGengo::Lite::Job->new_from_json_job($_) } @{$result->{data}->{jobs}}];
+    } elsif ($result->{data} and ref $result->{data} eq 'HASH' and
+             $result->{data}->{job} and ref $result->{data}->{job} eq 'HASH') {
+        $result->{jobs} = [WebService::myGengo::Lite::Job->new_from_json_job($result->{data}->{job})];
     }
     
     return bless $result, $class;
@@ -290,14 +321,6 @@ sub data {
 
 sub jobs {
     return $_[0]->{jobs};
-}
-
-sub job {
-    if ($_[0]->{data} and ref $_[0]->{data} eq 'HASH' and
-        $_[0]->{data}->{job} and ref $_[0]->{data}->{job} eq 'HASH') {
-        return bless {%{$_[0]->{data}->{job}}}, 'WebService::myGengo::Lite::Job';
-    }
-    return undef;
 }
 
 sub image_as_data_url {
